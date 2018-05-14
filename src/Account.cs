0ecfbf714ff703edced41e8c07bf50528b47b9c2
ben <ben@gladstone.digital>
2018-05-14 02:30:03 -0700

    oop optimization

--- src/Account.cs
+++ src/Account.cs
@@ -6,28 +6,32 @@ namespace Bank {
         public virtual int accountNumber { get; set; } = 0;
         public virtual decimal interestRate { get; set; } = 0.0m;
 
-        protected Account() {
+        protected Account(int accountNumber) {
+            this.accountNumber = accountNumber;
         }
 
-        protected Account(int accountNumber, Currency balance) : this() {
+        protected Account(int accountNumber, Currency balance) {
             this.balance = balance;
             this.accountNumber = accountNumber;
         }
     }
 
     public class CheckingAccount : Account {
+        public CheckingAccount(int accountNumber) : base(accountNumber) {}
         public CheckingAccount(int accountNumber, Currency balance) : base(accountNumber, balance) {}
     }
 
     public class SavingsAccount : Account {
         public override decimal interestRate { get; set; } = 0.10m;
 
+        public SavingsAccount(int accountNumber) : base(accountNumber) {}
         public SavingsAccount(int accountNumber, Currency balance) : base(accountNumber, balance) {}
     }
 
     public class MoneyMarketAccount : SavingsAccount {
         public override decimal interestRate { get; set; } = 0.20m;
 
+        public MoneyMarketAccount(int accountNumber) : base(accountNumber) {}
         public MoneyMarketAccount(int accountNumber, Currency balance) : base(accountNumber, balance) {}
     }
 }
--- src/Bank.cs
+++ src/Bank.cs
@@ -4,7 +4,7 @@ namespace Bank {
     public static class Program {
         public static void Main(string[] args) {
             Console.WriteLine("bankbois");
-            Account bob = new SavingsAccount(1415, new USD(12.14m));
+            Account bob = new SavingsAccount(1415);
             Console.WriteLine(bob.accountNumber + "\n" + bob.balance.quantity + "\n" + bob.interestRate);
             Account tim = new MoneyMarketAccount(12351, new USD(1154.01m));
             Console.WriteLine(tim.accountNumber + "\n" + tim.balance.quantity + "\n" + tim.interestRate);

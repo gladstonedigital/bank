15aff6913675cfcf4006cbd2663f87fa6a80676b
ben <ben@gladstone.digital>
2018-05-14 18:40:41 -0700

    reorganized

--- src/Account.cs
+++ src/Account.cs
@@ -4,7 +4,6 @@ namespace Bank {
     public abstract class Account {
         public virtual Currency balance { get; set; } = new USD();
         public virtual int accountNumber { get; set; } = 0;
-        public virtual decimal interestRate { get; set; } = 0.0m;
 
         protected Account(int accountNumber) {
             this.accountNumber = accountNumber;
@@ -25,7 +24,20 @@ namespace Bank {
             }
         }
 
-        public abstract bool withdraw(Currency amount);
+        public virtual bool withdraw(Currency amount) {
+            if (!String.Equals(balance.symbol, amount.symbol)) {
+                Console.WriteLine("Account currency and withdraw currency do not match; withdraw failed.");
+                return false;
+            }
+
+            if (balance.quantity - amount.quantity < 0.0m) {
+                Console.WriteLine("Insufficient funds.");
+                return false;
+            }
+
+            balance.quantity -= amount.quantity;
+            return true;
+        }
 
         public virtual bool transfer(Account from, Currency amount) {
             if (!String.Equals(balance.symbol, from.balance.symbol) || !String.Equals(balance.symbol, amount.symbol)) {
@@ -49,71 +61,57 @@ namespace Bank {
     }
 
     public class CheckingAccount : Account {
+        public const decimal overdraftFee = 20.00m;
         public CheckingAccount(int accountNumber) : base(accountNumber) {}
         public CheckingAccount(int accountNumber, Currency balance) : base(accountNumber, balance) {}
 
         public override bool withdraw(Currency amount) {
-            if (String.Equals(balance.symbol, amount.symbol)) {
-                if (balance.quantity - amount.quantity >= 0.0m) {
-                    balance.quantity -= amount.quantity;
-                    return true;
-                } else if (balance.quantity - amount.quantity - 20.0m >= -200.0m) {
-                    balance.quantity -= 20.0m + amount.quantity;
-                    Console.WriteLine("Overdraft; assessed $20.00 fee.");
-                    return true;
-                } else {
-                    Console.WriteLine("Insufficient funds.");
-                    return false;
-                }
-
-            } else {
+            if (!String.Equals(balance.symbol, amount.symbol)) {
                 Console.WriteLine("Account currency and withdraw currency do not match; withdraw failed.");
+                return false;
+            }
+
+            if (balance.quantity - amount.quantity >= 0.0m) {
+                balance.quantity -= amount.quantity;
+                return true;
+            } else if (balance.quantity - amount.quantity - 20.0m >= -200.0m) {
+                balance.quantity -= 20.0m + amount.quantity;
+                Console.WriteLine("Overdraft; assessed $20.00 fee.");
+                return true;
+            } else {
+                Console.WriteLine("Insufficient funds.");
                 return false;
             }
         }
     }
 
     public class SavingsAccount : Account {
-        public override decimal interestRate { get; set; } = 0.10m;
+        public virtual decimal interestRate { get; set; } = 0.10m;
 
         public SavingsAccount(int accountNumber) : base(accountNumber) {}
         public SavingsAccount(int accountNumber, Currency balance) : base(accountNumber, balance) {}
-
-        public override bool withdraw(Currency amount) {
-            if (String.Equals(balance.symbol, amount.symbol)) {
-                if (balance.quantity - amount.quantity >= 0.0m) {
-                    balance.quantity -= amount.quantity;
-                    return true;
-                } else {
-                    Console.WriteLine("Insufficient funds.");
-                    return false;
-                }
-            } else {
-                Console.WriteLine("Account currency and withdraw currency do not match; withdraw failed.");
-                return false;
-            }
-        }
-    }
+   }
 
     public class MoneyMarketAccount : SavingsAccount {
         public override decimal interestRate { get; set; } = 0.20m;
+        public const decimal withdrawFee = 1.50m;
 
         public MoneyMarketAccount(int accountNumber) : base(accountNumber) {}
         public MoneyMarketAccount(int accountNumber, Currency balance) : base(accountNumber, balance) {}
 
         public override bool withdraw(Currency amount) {
-            if (String.Equals(balance.symbol, amount.symbol)) {
-                if (balance.quantity - 1.50m - amount.quantity >= 0.0m) {
-                    balance.quantity -= 1.50m + amount.quantity;
-                    return true;
-                } else {
-                    Console.WriteLine("Insufficient funds.");
-                    return false;
-                }
-            } else {
+            if (!String.Equals(balance.symbol, amount.symbol)) {
                 Console.WriteLine("Account currency and withdraw currency do not match; withdraw failed.");
                 return false;
             }
+
+            if (balance.quantity - withdrawFee - amount.quantity < 0.0m) {
+                Console.WriteLine("Insufficient funds.");
+                return false;
+            }
+
+            balance.quantity -= withdrawFee + amount.quantity;
+            return true;
         }
     }
 }
